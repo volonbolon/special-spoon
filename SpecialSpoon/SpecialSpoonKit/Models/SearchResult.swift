83f62b2ed661556d5bcda498a3a01d0f67c2db1e
Ariel Rodriguez <ariel@volonbolon.me>
2021-01-20 23:05:38 -0300

    Add collectionViewUrl to search result model

--- SpecialSpoon/SpecialSpoonKit/Models/SearchResult.swift
+++ SpecialSpoon/SpecialSpoonKit/Models/SearchResult.swift
@@ -15,10 +15,12 @@ struct SearchResult: Codable, Identifiable {
     let id: Int
     let trackName: String
     let previewUrl: String
+    let collectionViewUrl: String
     
     private enum CodingKeys: String, CodingKey {
         case id = "trackId"
         case trackName
         case previewUrl
+        case collectionViewUrl
     }
 }
